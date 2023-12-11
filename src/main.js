ff6e34d2ceff400f323e25f9aba8453f7d603c4f
Vladimir Pobedinskiy <vladpob058@gmail.com>
2023-12-11 19:30:40 +0300

    @iconify/vue start.

--- src/main.js
+++ src/main.js
@@ -5,13 +5,15 @@ import { router } from './router'
 import { store } from './store'
 import './assets/scss/main.scss'
 
-import SwiperPlugin from './plugins/swiper'
+import '@/plugins/iconify-vue'
+import SwiperPlugin from '@/plugins/swiper'
 import { createVfm } from 'vue-final-modal'
 import 'vue-final-modal/style.css'
 
 // Глобальные компоненты
 import UIButton from '@/components/UI/UIButton'
 import UILink from '@/components/UI/UILink'
+import UIIcon from '@/components/UI/UIIcon'
 
 const app = createApp(App)
 app.use(router)
@@ -28,5 +30,6 @@ app.use(vfm)
 // Регистрируем глобальные компоненты
 app.component('UIButton', UIButton)
 app.component('UILink', UILink)
+app.component('UIIcon', UIIcon)
 
 app.mount('#app')
